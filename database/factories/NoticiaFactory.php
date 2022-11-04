e41f2e14c8e777169e31b27c9280ee4712700c34
Pablopg06 <pablopg0694@gmail.com>
2022-11-04 10:10:31 -0300

    Login basico

---
+++ database/factories/NoticiaFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Noticia>
+ */
+class NoticiaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $titulo = $this->faker->unique()->sentence();
+        return [
+            'epigrafe'=>$this->faker->sentence(),
+            'titulo'=>$titulo,
+            'slug'=>Str::slug($titulo,'-'),
+            'subtitulo'=>$this->faker->sentence(),
+            'cuerpo'=>$this->faker->paragraph(),
+            'imagen'=>$this->faker->randomElement(['http://localhost/img/image1.jpg',
+            'http://localhost/img/image2.jpg','http://localhost/img/image3.jpg']),
+            'piefoto'=>$this->faker->sentence()
+        ];
+    }
+}
---
+++ database/migrations/2022_11_02_163040_create_noticias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noticias', function (Blueprint $table) {
+            $table->id();
+            $table->string('epigrafe');
+            $table->string('titulo');
+            $table->string('subtitulo');
+            $table->text('cuerpo');
+            $table->string('imagen');
+            $table->string('piefoto');
+            $table->string('slug');
+            $table->boolean('publicar')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noticias');
+    }
+};
